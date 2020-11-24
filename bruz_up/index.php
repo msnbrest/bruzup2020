9f237410c74dab0d0c69ceed5147c0e1e1962e90
msnbrest <msnbrest@hotmail.fr>
2020-11-24 03:05:10 +0100

    lite co 202011240259

--- bruz_up/index.php
+++ bruz_up/index.php
@@ -6,14 +6,13 @@ require "../prive_sql/sql_controller.php";
 try {
 	$request = array_merge($_GET, $_POST);
 	$controller = new sql_app\Controller();
-	$controller->setRequest($request);
+
 	$request["page_precise"]="page_public_index";
 
 	//if (isset($request['action']) && $request['action'] != ""){
 		// si from action
 		//$action = $request['action'];
 		//$controller->$action($request);
-		//$controller->include_page(str_replace('..','',$action),$request);
 	//} else {
 		// sinon default
 		$controller->index($request);
--- prive_sql/sql_controller.php
+++ prive_sql/sql_controller.php
@@ -10,12 +10,6 @@ use PDOException;
 
 class Controller
 {
-    // Requête entrante
-    protected $request = array();
-
-    // Action à réaliser
-    protected $action = array();
-
     protected $file = "";
 
     // Fonction qui appelle la vue et y envoi les données
@@ -34,13 +28,6 @@ class Controller
         } else {
             throw new Exception("Fichier '$file' introuvable");
         }
-    }
-
-
-    // Mémorisation de la requête entrante
-    public function setRequest($request)
-    {
-        $this->request = $request;
     }
 
     public function add(array $param)
@@ -78,7 +65,9 @@ class Controller
                 //$param["password"].=$password_add;// add pour se co sur vrai bdd
                 $datas = $sellersRepository->getAllSellers();
                 $this->include_page("page_admin", $datas);
-            }
+            } else {
+					header("Location: /admin_bdd/index.php");
+				}
         } else {
             header("Location: /admin_bdd/index.php");
         }
@@ -99,36 +88,34 @@ class Controller
 
 // - - -
 
-class Database
-{
-    protected $connection;
-
-    public function __construct()
-    {
-        try {
-				 $this->connection = new PDO(
-					  'mysql:host='.$login.'.mysql.db;dbname='.$login,
-					  $login,
-					  $password.$password_add,
-					  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-}
-
-// - - -
-
 /*
+chaque commerçant a : id(AI), nom, tel, mdp, commerce, adresse
 	#	Nom 	Type	Interclassement	Attributs	Null	Valeur par défaut	Commentaires	Extra	Action
 	1	idPrimaire	int(3)		UNSIGNED	Non	Aucun(e)		AUTO_INCREMENT	Modifier Modifier	Supprimer Supprimer	
 	2	nom	varchar(50)	utf8_bin		Oui	NULL			Modifier Modifier	Supprimer Supprimer	
 */
 
-class SellersRepository extends Database
+class SellersRepository
 {
+    protected $connection;
+
+    public function __construct()
+    {
+		try {
+
+			include("../prive/mdp.php");// $login = ''; $password = ''; $password_add = "";
+
+			$this->connection = new PDO(
+			'mysql:host='.$login.'.mysql.db;dbname='.$login,
+			$login,
+			$password.$password_add,
+			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+    }
 
     public function getAllSellers()
     {
@@ -151,29 +138,8 @@ class SellersRepository extends Database
 
     public function addSeller(array $param)
     {
-/*
-        $query = "INSERT INTO sellers(seller_name, seller_fisrtname, company, adress, postal_code, city, 
-                    product, category, phone, cellphone, mail, website, social_media, picture) 
-                    VALUES(:seller_name, :seller_fisrtname, :company, :adress, :postal_code, :city, :product,
-                           :category, :phone, :cellphone, :mail, :website, :social_media, :picture)";
-        $res = $this->connection->prepare($query);
-        $res->bindValue(':seller_name', $param['seller_name'], PDO::PARAM_STR);
-        $res->bindValue(':seller_fisrtname', $param['seller_fisrtname'], PDO::PARAM_STR);
-        $res->bindValue(':company', $param['company'], PDO::PARAM_STR);
-        $res->bindValue(':adress', $param['adress'], PDO::PARAM_STR);
-        $res->bindValue(':postal_code', $param['postal_code'], PDO::PARAM_STR);
-        $res->bindValue(':city', $param['city'], PDO::PARAM_STR);
-        $res->bindValue(':product', $param['product'], PDO::PARAM_STR);
-        $res->bindValue(':category', $param['category'], PDO::PARAM_STR);
-        $res->bindValue(':phone', $param['phone'], PDO::PARAM_STR);
-        $res->bindValue(':cellphone', $param['cellphone'], PDO::PARAM_STR);
-        $res->bindValue(':mail', $param['mail'], PDO::PARAM_STR);
-        $res->bindValue(':website', $param['website'], PDO::PARAM_STR);
-        $res->bindValue(':social_media', $param['social_media'], PDO::PARAM_STR);
-        $res->bindValue(':picture', $param['picture'], PDO::PARAM_STR);
-*/
         $query = "INSERT INTO anc_commercants(nom) 
-                    VALUES(:nom)";
+            VALUES(:nom)";
         $res = $this->connection->prepare($query);
         $res->bindValue(':nom', $param['form_nom'], PDO::PARAM_STR);
         $res->execute();
@@ -181,31 +147,8 @@ class SellersRepository extends Database
 
     public function updateSeller(array $param)
     {
-/*
-        $query = "UPDATE sellers SET seller_name = :seller_name, seller_fisrtname = :seller_fisrtname, 
-                   company = :company, adress = :adress, postal_code = :postal_code, city = :city,
-                   product = :product, category = :category, phone = :phone, cellphone = :cellphone,
-                   mail = :mail, website = :website, social_media = :social_media, picture = :picture
-                   WHERE id = :id";
-        $res = $this->connection->prepare($query);
-        $res->bindValue(':id', $param['id'], PDO::PARAM_INT);
-        $res->bindValue(':seller_name', $param['seller_name'], PDO::PARAM_STR);
-        $res->bindValue(':seller_fisrtname', $param['seller_fisrtname'], PDO::PARAM_STR);
-        $res->bindValue(':company', $param['company'], PDO::PARAM_STR);
-        $res->bindValue(':adress', $param['adress'], PDO::PARAM_STR);
-        $res->bindValue(':postal_code', $param['postal_code'], PDO::PARAM_STR);
-        $res->bindValue(':city', $param['city'], PDO::PARAM_STR);
-        $res->bindValue(':product', $param['product'], PDO::PARAM_STR);
-        $res->bindValue(':category', $param['category'], PDO::PARAM_STR);
-        $res->bindValue(':phone', $param['phone'], PDO::PARAM_STR);
-        $res->bindValue(':cellphone', $param['cellphone'], PDO::PARAM_STR);
-        $res->bindValue(':mail', $param['mail'], PDO::PARAM_STR);
-        $res->bindValue(':website', $param['website'], PDO::PARAM_STR);
-        $res->bindValue(':social_media', $param['social_media'], PDO::PARAM_STR);
-        $res->bindValue(':picture', $param['picture'], PDO::PARAM_STR);
-*/
         $query = "UPDATE anc_commercants SET nom = :nom
-                   WHERE id = :id";
+            WHERE id = :id";
         $res = $this->connection->prepare($query);
         $res->bindValue(':id', $param['form_id'], PDO::PARAM_INT);
         $res->bindValue(':nom', $param['form_nom'], PDO::PARAM_STR);
@@ -214,7 +157,7 @@ class SellersRepository extends Database
     }
 
 	public function deleteSeller(array $param){
-		if(isset($param['form_id'])||isset($param['form_id'])<0){
+		if(isset($param['form_id']) || isset($param['form_id'])<0){
 			$query = "DELETE FROM anc_commercants WHERE id = :id";
 			$res = $this->connection->prepare($query);
 			$res->bindValue(':id', $param['form_id'], PDO::PARAM_INT);
